329aab895a2e4b38f102e449a947fc9b5d10d3d7
Ossy <drake@OSSMAN>
2025-11-02 13:02:53 +0100

    Fix: Routing problem with booking controller. Better works with validation
    feat: Created placeholder for confrmed booking

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pegasus_MVC.Models;
 
 namespace Pegasus_MVC.Controllers
 {
@@ -6,7 +7,27 @@ namespace Pegasus_MVC.Controllers
     {
         public IActionResult Index()
         {
+            return View(new CreateBookingVM());
+        }
+        public IActionResult ConfirmedBooking()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateBookingVM createBooking)
+        {
+            if(createBooking.PickUpDateTime < DateTime.UtcNow.AddHours(48))
+            {
+                ModelState.AddModelError(nameof(createBooking.PickUpDateTime),
+                    "Pickup date must be at least 48 hours from now.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View("Index", createBooking);
+            }
+
+            return RedirectToAction("ConfirmedBooking");
         }
     }
 }
---
+++ Models/CreateBookingVM.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pegasus_MVC.Models
+{
+    public class CreateBookingVM
+    {
+        [Required(ErrorMessage = "Need a First name")]
+        public string FirstName { get; set; } = null!;
+        [Required(ErrorMessage = "Need a last name")]
+        public string LastName { get; set; } = null!;
+        [Required(ErrorMessage = "Need a Email")]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "Need a Phonenumber")]
+        [Phone]
+        public string PhoneNumber { get; set; } = null!;
+        [Required(ErrorMessage = "Need a pickup time")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime PickUpDateTime { get; set; } = DateTime.UtcNow;
+        [Required(ErrorMessage = "Need a pickup addres")]
+        public string PickUpAddress { get; set; } = null!;
+        public string? FirstStop {  get; set; }
+        public string? SecStop {  get; set; }
+        public string? ThirdStop {  get; set; }
+        [Required(ErrorMessage = "Need a dropoff addres")]
+        public string DropOffAddres { get; set; } = null!;
+        public string? Flightnumber { get; set; }
+        [StringLength(300)]
+        public string? Comment { get; set; }
+        public decimal Price { get; set; }
+    }
+}
